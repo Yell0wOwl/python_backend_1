1af318fdd65d6dcf771b6ff22cefded36902a7d4
Yell0wOwl <popovg2001@mail.ru>
2021-10-25 21:45:06 +0300

    1.1

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
-import sys
 import unittest
+from unittest.mock import MagicMock
 from main import TicTac
+
 
 class Test(unittest.TestCase):
     def setUp(self):
@@ -9,7 +10,7 @@ class Test(unittest.TestCase):
         self.game.FIRST_NAME = 'Player1'
         self.game.SECOND_NAME = 'Player2'
         self.game.WIN_SIZE = 3
-        self.game.board = [['X', '*', '*'], ['0', 'X', 'X'], ['*', '*', 'X']]
+        self.game.board = [['X', '*', '*'], ['O', 'X', 'X'], ['*', '*', 'X']]
 
     def test_show_board(self):
         text_board = '''   1 2 3
@@ -19,6 +20,16 @@ class Test(unittest.TestCase):
         self.assertEqual(self.game.show_board(), print(text_board))
 
     def test_check_winner(self):
-        self.assertEqual(self.game.check_winner(1,[2,2]),sys.exit("Конец игры!"))
+        self.assertEqual(self.game.check_winner(0, [2, 2]), True)
+
+    def test_turn(self):
+        self.game.board = [['X', '*', '*'], ['*', '*', 'X'], ['*', '*', 'X']]
+        true_board = [['X', '*', '*'], ['*', 'X', 'X'], ['*', '*', 'X']]
+        self.game.get_coordinates = MagicMock(return_value=[1, 1])
+        self.game.turn_counter = 3
+        self.game.turn(0)
+        self.assertEqual(self.game.board, true_board)
+
+
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 
 class TicTac:
@@ -6,8 +7,10 @@ class TicTac:
     global WIN_SIZE
     global SECOND_NAME
     global FIRST_NAME
+    global turn_counter
     global board
-    
+
+    # Выводит в консоль игровую доску
     def show_board(self):
         print('  ', end='')
         for i in range(self.SIZE):
@@ -17,24 +20,34 @@ class TicTac:
             for j in range(self.SIZE):
                 print(' ', self.board[i][j], end='')
 
+    # Ход игрока. Ставит символ в выбранном месте.
     def turn(self, player):
-        if player == 1:
+        if self.turn_counter == self.SIZE ** 2:
+            self.show_board()
+            time.sleep(0.5)
+            sys.exit('\nНичья!')
+        if player == 0:
+            symbol = 'X'
             print('Ход игрока', self.FIRST_NAME, '\n')
-            self.show_board()
-            print('\nВведите номер строки и столбца, чтобы поставить туда X:')
-            coordinates = self.paint()
-            self.board[coordinates[0]][coordinates[1]] = 'X'
-            self.check_winner(1, coordinates)
-            self.turn(2)
-        if player == 2:
+        else:
+            symbol = 'O'
             print('Ход игрока', self.SECOND_NAME, '\n')
-            self.show_board()
-            print('\nВведите номер строки и столбца, чтобы поставить туда O:')
-            coordinates = self.paint()
-            self.board[coordinates[0]][coordinates[1]] = 'O'
-            self.check_winner(2, coordinates)
-            self.turn(1)
+        self.show_board()
+        print(f'\nВведите номер строки и столбца, чтобы поставить туда {symbol}:')
+        while True:
+            coordinates = self.get_coordinates()
+            if self.board[coordinates[0]][coordinates[1]] == '*':
+                break
+            print('Выберите свободное поле!')
+        self.board[coordinates[0]][coordinates[1]] = symbol
+        self.turn_counter += 1
+        win = self.check_winner(player, coordinates)
+        if not win:
+            self.turn(not player)
 
+    # Старт игры. Выбор размеров поля, необходимой
+    # для победы длины линии, имен игроков.
+    # Создание поля.
     def start_game(self):
         print('Введите размер поля: ')
         try:
@@ -51,9 +64,11 @@ class TicTac:
         print('Введите имя второго игрока:')
         self.SECOND_NAME = input()
         self.board = [['*' for i in range(self.SIZE)] for j in range(self.SIZE)]
-        self.turn(1)
+        self.turn_counter = 0
+        self.turn(0)
 
-    def paint(self):
+    # Получения координат выбранной игроком точки.
+    def get_coordinates(self):
         is_valid = 0
         coordinates = [-1, -1]
         while is_valid == 0:
@@ -71,10 +86,11 @@ class TicTac:
                 continue
         return coordinates
 
+    # Проверка одной из выигрышных комбинаций.
     def check_line(self, player, coordinates, dy, dx):
         def requisition(next_coord_y, next_coord_x):
             return next_coord_x < 0 or next_coord_x >= self.SIZE or next_coord_y < 0 or next_coord_y >= self.SIZE
-        if player == 1:
+        if player == 0:
             symbol = 'X'
         else:
             symbol = 'O'
@@ -98,17 +114,23 @@ class TicTac:
                 if requisition(next_coord_y, next_coord_x):
                     break
         if counter >= self.WIN_SIZE:
-            if player == 1:
-                print("Победил", self.FIRST_NAME)
+            self.show_board()
+            if player == 0:
+                print("\nПобедил", self.FIRST_NAME)
             else:
-                print("Победил", self.SECOND_NAME)
-            sys.exit("Конец игры!")
+                print("\nПобедил", self.SECOND_NAME)
+            return 1
+        return 0
 
+    # Проверка условий победы. Ищет на поле все
+    # возможные выигрышные комбинации.
     def check_winner(self, player, coordinates):
-        self.check_line(player, coordinates, 1, 1)
-        self.check_line(player, coordinates, 1, -1)
-        self.check_line(player, coordinates, 1, 0)
-        self.check_line(player, coordinates, 0, 1)
+        win = 0
+        win += self.check_line(player, coordinates, 1, 1)
+        win += self.check_line(player, coordinates, 1, -1)
+        win += self.check_line(player, coordinates, 1, 0)
+        win += self.check_line(player, coordinates, 0, 1)
+        return win
 
 
 if __name__ == '__main__':
